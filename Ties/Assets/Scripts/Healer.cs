c32d76d8600a6f4353575bc498a091e6d680f9fa
h2oyo <sonofhoddy@hotmail.com>
2016-06-13 17:41:59 -0700

    heals

---
+++ Ties/Assets/Scripts/Healer.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Healer : MonoBehaviour
+{
+    public bool healerMenu;
+    WarriorClass warrior;
+    Transform player;
+    // Use this for initialization
+
+    private float Distance()
+    { return Vector3.Distance(player.position, gameObject.transform.position); }
+    void OnMouseOver()
+    {
+
+        if (Input.GetMouseButtonDown(0) && Distance() < 3)
+            healerMenu = true;
+    }
+    void Start()
+    {
+        healerMenu = false;
+
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        warrior = player.GetComponent<WarriorClass>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+
+    }
+}
